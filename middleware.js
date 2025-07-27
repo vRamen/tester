220502d9aa59486a8e456811dc2a87850842cd64
akaucup <40891013+akaucup@users.noreply.github.com>
2025-07-27 11:53:41 +0700

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,12 @@
+export function middleware(request) {
+  const hostname = request.headers.get("host") || "";
+
+  const mapping = {
+    "kamingundang.biz.id": "/template1/index.html",
+    "contohdomainlain.id": "/template2/index.html"
+  };
+
+  const destination = mapping[hostname.toLowerCase()] || "/";
+
+  return Response.rewrite(new URL(destination, request.url));
+}
